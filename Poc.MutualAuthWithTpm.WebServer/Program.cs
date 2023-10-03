9b2fc4fff34b6921a4e5c9be358f3329a113f80b
Andrew Potapov <andrew.potapov@gmail.com>
2023-10-03 14:42:29 +0300

    Add Poc.MutualAuthWithTpm.WebServer

--- Poc.MutualAuthWithTpm.WebServer/Program.cs
+++ Poc.MutualAuthWithTpm.WebServer/Program.cs
@@ -1,7 +1,4 @@
 
-using Microsoft.AspNetCore.Server.Kestrel.Https;
-using System.Security.Cryptography.X509Certificates;
-
 namespace Poc.MutualAuthWithTpm.WebServer
 {
     public class Program
@@ -10,33 +7,9 @@ namespace Poc.MutualAuthWithTpm.WebServer
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // load cert from pfx
-            //var certificate = new X509Certificate2(
-            //    "C:/Cert/TpmServerTestAuthCert.pfx",
-            //    "TpmServer"
-            //);
-
-            var store = new X509Store(StoreName.Root, StoreLocation.CurrentUser);
-            store.Open(OpenFlags.ReadOnly);
-            var certificate = store.Certificates.OfType<X509Certificate2>()
-                .First(c => c.FriendlyName == "TPM Server Test-only Authorization Certificate");
-
-            builder.WebHost.UseKestrel(options =>
-            {
-                options.Listen(System.Net.IPAddress.Loopback, 44321, listenOptions =>
-                {
-                    var connectionOptions = new HttpsConnectionAdapterOptions
-                    {
-                        ServerCertificate = certificate
-                    };
-
-                    listenOptions.UseHttps(connectionOptions);
-                });
-            });
-
             // Add services to the container.
-            builder.Services.AddAuthorization();
 
+            builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
@@ -55,9 +28,7 @@ namespace Poc.MutualAuthWithTpm.WebServer
             app.UseAuthorization();
 
 
-            app.MapGet("/ping", () => Results.Ok())
-            .WithName("Ping")
-            .WithOpenApi();
+            app.MapControllers();
 
             app.Run();
         }
